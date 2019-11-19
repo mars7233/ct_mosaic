a05c6691112d60d84b4bf23387855757ea86038b
mars7233 <mars2022@vip.qq.com>
2019-11-19 16:35:22 +0800

    Modify：修改ROI区域，增加main函数

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ def cosine(image1, image2):
     return pdist(X, 'cosine')[0]
 
 
-def getImg(path):
+def get_img(path):
     img_list = []
     counter = 0
     list_dir = sorted(os.listdir(path))
@@ -26,32 +26,34 @@ def holder_clear(imglist):
     face_list = []
     counter = 0
     for item in imglist:
-        face_list.insert(counter, item[165:295, 145:295])
+        face_list.insert(counter, item[165:290, 155:285])
         counter = counter + 1
     return face_list
 
 
-# 文件导入
-img_list1 = getImg('CT/Sample_1_20190818_131304')
-img_list2 = getImg('CT/Sample_1_20190818_134119')
-print(img_list2[1].shape)
-
-# 去容器
-img_list11 = holder_clear(img_list1)
-img_list22 = holder_clear(img_list2)
-
-# cv2.imshow("demo1", img_list11[200])
-# cv2.imshow("demo2", img_list22[2])
-
-# 亮度均衡
+def organize_img():
+    return
 
 
-# 相似度计算
-# print("余弦相似度：%f" % (cosine(image1, image2)))
+if __name__ == "__main__":
+    # 文件导入
+    img_list1 = get_img('CT/Sample_1_20190818_131304')
+    img_list2 = get_img('CT/Sample_1_20190818_134119')
+    print(img_list2[1].shape)
 
-# 文件重排
+    # 去容器
+    img_list11 = holder_clear(img_list1)
+    img_list22 = holder_clear(img_list2)
 
+    cv2.imshow("demo1", img_list11[200])
+    cv2.imshow("demo2", img_list22[2])
 
+    # 亮度均衡
 
-cv2.waitKey(111)
-cv2.destroyAllWindows()
+    # 相似度计算
+    # print("余弦相似度：%f" % (cosine(image1, image2)))
+
+    # 文件重排
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
